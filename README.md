ad9494633d07ef962daf119dd2b0c6fc32762038
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-02-20 19:03:34 -0500

    npm env installed and apply

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 #  Acme Human Resources (HR) 
 ###  build an API that the Acme Human Resources (HR) Department can use to manage its employees and departments. A department can have many employees, and an employee must belong to a department.
 
+
 ![alt text](img/image.png)
+
+### Setting up .env for the Project
+- npm install dotenv
+- create a .env file
+- Create a .env file in the root directory of the project <br> 
+  replacing the values with your own PostgreSQL environment details: 
+- require('dotenv').config({ path: `${__dirname}/your_path ` });
+
+```
+PGUSER=your_postgres_username      # Replace with your PostgreSQL username
+PGHOST=localhost                   # Leave as localhost unless your database is hosted elsewhere
+PGDATABASE=your_database_name      # Replace with the name of your PostgreSQL database
+PGPASSWORD=your_password           # Replace with your PostgreSQL password
+PGPORT=5432                        # Default PostgreSQL port (use 5432 unless it's different)
+```
+
--- client.js
+++ client.js
@@ -1,6 +1,35 @@
-const { Client } = require('pg');
-const port = 5432;
-const client = new Client (`postgres://localhost:${port}/`);
+require('dotenv').config({ path: `${__dirname}/env/.env` });
+
+const { Client } = require("pg");
+
+//TESTING 
+// console.log(__dirname);
+// console.log('PGHOST:', process.env.PGHOST);
+// console.log('PGDATABASE:', process.env.PGDATABASE);
+// console.log('PGPASSWORD:', process.env.PGPASSWORD);
+// console.log('PGPORT:', process.env.PGPORT);
+
+const client = new Client({
+  user: process.env.PGUSER,        // PostgreSQL username
+  host: process.env.PGHOST,        // PostgreSQL server host
+  database: process.env.PGDATABASE,// PostgreSQL database
+  password: process.env.PGPASSWORD,// PostgreSQL password
+  port: process.env.PGPORT         // PostgreSQL port 
+  
+});
+
+const clientConnecting = async () => {
+  try {
+    await client.connect();
+    console.log("Client connected......✅");
+  } catch (err) {
+    console.log("Error connecting:", err, '⚠️');
+  } finally {
+    await client.end();
+    console.log("Client disconnected ......❌");
+  }
+};
+
+clientConnecting();
+
 module.exports = client;
-
-
