e9aef16779d5e3bbcd6a2faf60f6ccdf084dbf6a
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-02-22 03:41:14 -0500

    adding fetch | update | delete |show all features to the employee and the seed files

--- employee.cjs
+++ employee.cjs
@@ -1,17 +1,52 @@
-const client = require('./client.cjs');
-const { addDepartment } = require('./department.cjs');
+const client = require("./client.cjs");
 
-const addEmployee = async (name,departmentId) => {
-  try {    
+const addEmployee = async (name, departmentId) => {
+  try {
     await client.query(
-      'INSERT INTO employee(name, department_id) VALUES($1,$2)',
-      [name,departmentId]
+      "INSERT INTO employee(name, department_id) VALUES($1, $2)",
+      [name, departmentId]
     );
-    console.log('Employee table : \n INSERT INTO employee [ ${name} ] successfully........ ✅');
+    console.log(
+      `Employee table: \n INSERT INTO employee [ ${name} ] successfully........ ✅`
+    );
   } catch (err) {
     console.log("Error adding employee: ⚠️", err);
-  } 
+  }
 };
 
+const fetchAllEmployees = async () => {
+  try {
+    const { rows: fetchEmployee } = await client.query("SELECT * FROM employee");
+    return fetchEmployee; 
+  } catch (err) {
+    console.log("Error fetching employees: ⚠️", err);
+  }
+};
 
-module.exports = { addEmployee };  
+const updateEmployee = async (id, name, departmentId) => {
+  try {
+    const { rows: updatedEmployee } = await client.query(
+      "UPDATE employee SET name=$1, department_id=$2 WHERE id=$3 RETURNING *",
+      [name, departmentId, id]
+    );
+    console.log(`Employee [ ${id} ] updated successfully........ ✅`);
+    return updatedEmployee;
+  } catch (err) {
+    console.log("Error updating employee: ⚠️", err);
+  }
+};
+
+const deleteEmployee = async (id) => {
+  try {
+    const { rows: deletedEmployee } = await client.query(
+      "DELETE FROM employee WHERE id=$1 RETURNING *",
+      [id]
+    );
+    console.log(`Employee [ ${id} ] deleted successfully........ ✅`);
+    return deletedEmployee;
+  } catch (err) {
+    console.log("Error deleting employee: ⚠️", err);
+  }
+};
+
+module.exports = { addEmployee, fetchAllEmployees, updateEmployee, deleteEmployee };
